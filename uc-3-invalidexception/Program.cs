bb5b700d485fcefb39efcb61688db5fce99f6913
srikala <vlslaksh1899@gmail.com>
2022-04-20 15:56:27 +0530

    added uc-3 invalid exception

---
+++ uc-3-invalidexception/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace Mood_Analyzer_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO MOOD ANALYZER PROGRAM");
+
+        }
+    }
+}
---
+++ uc-3-invalidexception/testmood/UnitTest1.cs
@@ -0,0 +1,57 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mood_Analyzer_Program;
+namespace Mood_Analyzer_Test
+{
+    [TestClass]
+    public class TestMood
+    {
+        [TestMethod]
+        public void NullMood()
+        {
+            string msg = " ";
+            string expected = "HAPPY";
+
+            MoodAnalyzer mood = new MoodAnalyzer(msg);
+
+            string actual = mood.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void getCustomNullException()
+        {
+            string expected = "message should not be null";
+            MoodAnalyzer modeAnalyzer = new MoodAnalyzer(" ");
+
+            try
+            {
+                string actual = modeAnalyzer.AnalyseMood();
+
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void getCustomEmptyException()
+        {
+            string expected = "message should not be empty";
+            MoodAnalyzer modeAnalyzer = new MoodAnalyzer(string.Empty);
+
+            try
+            {
+                string actual = modeAnalyzer.AnalyseMood1();
+
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+
+        }
+    }
+}
