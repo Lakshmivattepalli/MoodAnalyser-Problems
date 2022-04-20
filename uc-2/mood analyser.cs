45beadf8ac2c59072bff24dfe20ceb63cd0c16f3
srikala <vlslaksh1899@gmail.com>
2022-04-20 15:45:08 +0530

    added uc-2 exception handling

---
+++ uc-2/mood analyser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mood_Analyzer_Program
+{
+    public class MoodAnalyzer
+    {
+        public string msg;
+        public MoodAnalyzer(string msg)
+        {
+            this.msg = msg;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (msg.ToLower().Contains(""))
+                    return "HAPPY";
+
+                else
+                    return "SAD";
+            }
+            catch (NullReferenceException msg)
+            {
+                return "Happy";
+            }
+        }
+    }
+}
