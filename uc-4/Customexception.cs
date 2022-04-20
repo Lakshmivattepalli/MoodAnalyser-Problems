52fcf729216e4dba542d71735f6e2db5d94dad2d
srikala <vlslaksh1899@gmail.com>
2022-04-20 16:11:33 +0530

    added uc-4 in the moodanalyser problem

---
+++ uc-4/Customexception.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mood_Analyzer_Program
+{
+    public class CustomException : Exception
+    {
+        public ExceptionType type;
+
+        public enum ExceptionType
+        {
+            Null_Type_Exception,
+            Empty_Type_Exception,
+            NO_SUCH_CLASS,
+            NO_SUCH_METHOD
+        }
+        public CustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
---
+++ uc-4/moodanalyser.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mood_Analyzer_Program
+{
+    public class MoodAnalyzer
+    {
+        public string msg;
+        public MoodAnalyzer(string msg)
+        {
+            this.msg = msg;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (msg.ToLower().Contains(""))
+                    return "HAPPY";
+
+                else
+                    return "SAD";
+            }
+            catch (NullReferenceException ex)
+            {
+                throw new CustomException(CustomException.ExceptionType.Null_Type_Exception, "Message should not be null");
+            }
+        }
+
+        public string AnalyseMood1()
+        {
+            try
+            {
+                if (msg.ToLower().Contains(string.Empty))
+                {
+                    return "happy";
+                }
+                else
+                    return "sad";
+            }
+            catch (NullReferenceException ex)
+            {
+                throw new CustomException(CustomException.ExceptionType.Empty_Type_Exception, "Message should not be empty");
+            }
+        }
+    }
+}
