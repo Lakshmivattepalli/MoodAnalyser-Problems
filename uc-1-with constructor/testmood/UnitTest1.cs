2ffdde99b30b5a7338bd90080fda61ec5ea7712b
srikala <vlslaksh1899@gmail.com>
2022-04-20 15:36:17 +0530

    added uc-1 case to mood analyser

---
+++ uc-1-with constructor/testmood/UnitTest1.cs
@@ -0,0 +1,34 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mood_Analyzer_Program;
+namespace Mood_Analyzer_Test
+{
+    [TestClass]
+    public class TestMood
+    {
+        [TestMethod]
+        public void MsgSadMood()
+        {
+            string msg = "I am in sad mood";
+            string expected = "SAD";
+
+            MoodAnalyzer mood = new MoodAnalyzer(msg);
+
+            string actual = mood.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void MsgHaapyMood()
+        {
+            string msg = "I am in Any mood";
+            string expected = "HAPPY";
+
+            MoodAnalyzer mood = new MoodAnalyzer(msg);
+
+            string actual = mood.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
